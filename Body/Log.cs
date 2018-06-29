e28cc8b59f8370a0f1ab42c49afc8c33a775c963
Sam Streed <embersteelstreus@gmail.com>
2018-06-29 11:20:51 -0500

    Modified project details. Added entrypoint Log class.

---
+++ Body/Log.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Dullahan
+{
+    /// <summary>
+    /// Entrypoint for sending logging information to clients.
+    /// </summary>
+    public class Log
+    {
+        #region STATIC_VARS
+
+        #endregion
+
+        #region INSTANCE_VARS
+
+        #endregion
+
+        #region STATIC_METHODS
+
+        #endregion
+
+        #region INSTANCE_METHODS
+
+        #endregion
+
+        #region INTERNAL_TYPES
+
+        #endregion
+    }
+}
